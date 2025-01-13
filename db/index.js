ca674636a6c2e96c3f42910a6ebcafe5e910fdf6
Swetha <swetha.muralitharan@axians.co.uk>
2025-01-13 10:56:01 +0000

    completes day 1 exercises

--- db/index.js
+++ db/index.js
@@ -1,3 +1,4 @@
+import { truncateSync } from 'fs'
 import knex from 'knex'
 import { fileURLToPath } from 'url'
 
@@ -5,3 +6,12 @@ import { fileURLToPath } from 'url'
  * TODO: Follow the steps required to connect to the database using knex
  * Make sure your default export is a const called db
  */
+
+const path = new URL('./db.sqlite', import.meta.url)
+const db = knex({
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    connection: { filename: fileURLToPath(path) }
+})
+
+export default db
